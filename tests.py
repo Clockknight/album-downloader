1d6e19ed435120d4b8111cddf7c247f167d8ce6f
Clockknight <34818025+Clockknight@users.noreply.github.com>
2023-04-21 13:36:59 -0700

    Cleanup notes

--- tests.py
+++ tests.py
@@ -1,30 +1,4 @@
-"""
-def result():
-    io = Information()
-    io.historystorage = checkhistory()
-    print(readhistory(io))
 
-    searchinput(0, "Ken Ashcorp")
-    searchinput(0, "Mystery Skulls")
-
-
-
-thing = input("You're running test.py. This is going to clear your history.json, press enter to continue.")
-
-testclear()
-
-searchinput(0, "crowder")
-result()
-
-searchinput(0, "Siames")
-result()
-
-urlinput("https://www.youtube.com/watch?v=BKUa0ISxhvQ")
-searchinput(1, "20 Percent Cooler")
-print(readhistory(checkhistory()))
-
-testclear()
-"""
 
 import unittest
 import time
@@ -32,38 +6,21 @@ from unittest import mock
 from scripts.userfunctions import *
 import mp3hash
 
-directories = [
-    "./Ken Ashcorp",
-    "/Various",
-    "./URL Downloads",
-    "./GGRIM",
-    "./Cxxlion"
-]
-
-'''h1 = hashlib.sha256(open(".\\assets\\Testing\\urlinputtest.mp3", "rb")).hexdigest()
-path = urlinput("https://www.youtube.com/watch?v=FVjeWMr8aLo")
-h2 = hashlib.sha256(open(path, "rb")).hexdigest()'''
-
-def hash(path):
-    return mp3hash.mp3hash(path)
-
-
-
 class UnitTestingEnvironment(unittest.TestCase):
     def test_unittest_environment(self):
         assert 1 == 1
 
     def test_hashing_method(self):
-        h1 = hash(".\\assets\\Testing\\urlinputtest.mp3")
-        h2 = hash(".\\assets\\Testing\\hashingtest.mp3")
+        h1 = mp3hash.mp3hash(".\\assets\\Testing\\urlinputtest.mp3")
+        h2 = mp3hash.mp3hash(".\\assets\\Testing\\hashingtest.mp3")
         assert h1 != h2
 
 class InputTests(unittest.TestCase):
     @staticmethod
     def test_urlinput():
-        h1 = hash(".\\assets\\Testing\\urlinputtest.mp3")
+        h1 = mp3hash.mp3hash(".\\assets\\Testing\\urlinputtest.mp3")
         path = urlinput("https://www.youtube.com/watch?v=FVjeWMr8aLo")
-        h2 = hash(path)
+        h2 = mp3hash.mp3hash(path)
         assert h1 == h2
 
     @mock.patch('scripts.userfunctions.input', create=True)
